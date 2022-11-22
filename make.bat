13f7662135e94e4f4e76890aa77cec25784e3dd2
Negative Charge <negativecharge6502@gmail.com>
2022-11-22 17:28:39 +0000

    Dynamically load sample and track data

--- make.bat
+++ make.bat
@@ -1,2 +1,14 @@
 @ECHO OFF
-BeebAsm.exe -i .\main.6502 -title SQUATM -do .\SquatM_Beeper_Engine.ssd -opt 3
+BeebAsm.exe -i .\tracks\shiru.lets_go.samples.6502
+BeebAsm.exe -i .\tracks\shiru.lets_go.track.6502
+
+BeebAsm.exe -i .\tracks\dj.h0ffman.squat_party.samples.6502
+BeebAsm.exe -i .\tracks\dj.h0ffman.squat_party.track.6502
+
+BeebAsm.exe -i .\tracks\shiru.geostorm.samples.6502
+BeebAsm.exe -i .\tracks\shiru.geostorm.track.6502
+
+BeebAsm.exe -i .\tracks\shiru.high_orbit.samples.6502
+BeebAsm.exe -i .\tracks\shiru.high_orbit.track.6502
+
+BeebAsm.exe -i .\main.s.6502 -title SQUATM -do .\SquatM_Beeper_Engine.ssd -opt 3
