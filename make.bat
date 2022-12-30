564957dc384fd51dcc2f85553b2cb01a1e76ba77
Negative Charge <negativecharge6502@gmail.com>
2022-12-30 13:42:12 +0000

    Fix issue with reuse of temporary variable

--- make.bat
+++ make.bat
@@ -1,14 +1,8 @@
 @ECHO OFF
-BeebAsm.exe -i .\tracks\shiru.lets_go.samples.6502
-BeebAsm.exe -i .\tracks\shiru.lets_go.track.6502
 
-BeebAsm.exe -i .\tracks\dj.h0ffman.squat_party.samples.6502
-BeebAsm.exe -i .\tracks\dj.h0ffman.squat_party.track.6502
+cd ".\tracks"
+del *.bin
+forfiles /s /m *.6502 /c "cmd /c BeebAsm.exe -i @path"
 
-BeebAsm.exe -i .\tracks\shiru.geostorm.samples.6502
-BeebAsm.exe -i .\tracks\shiru.geostorm.track.6502
-
-BeebAsm.exe -i .\tracks\shiru.high_orbit.samples.6502
-BeebAsm.exe -i .\tracks\shiru.high_orbit.track.6502
-
+cd ..
 BeebAsm.exe -i .\main.s.6502 -title SQUATM -do .\SquatM_Beeper_Engine.ssd -opt 3
